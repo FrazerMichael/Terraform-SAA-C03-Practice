026b46c44fce9d65fa5c8744c58085247b31d4d5
Michael Frazer <mjfrazer1@gmail.com>
2023-10-13 23:33:54 -0400

    Test ec2 config

---
+++ Basic-EC2/ec2/variables.tf
@@ -0,0 +1,19 @@
+variable "vpc_id" {
+    type = string
+}
+
+variable "ami_id" {
+    type = string
+}
+
+variable "key" {
+    type = string
+}
+
+variable "subnet_id" {
+    type = string
+}
+
+variable "SG" {
+    type = string
+}
