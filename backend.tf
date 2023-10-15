468f75008ee6baae0ee63b2c02bc20bf3c9bd0da
Michael Frazer <mjfrazer1@gmail.com>
2023-10-15 13:43:23 -0400

    removed extra backend and provider

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    profile = "iamadmin-general"
-    bucket  = "generalaccount-tfstate-bucket"
-    key     = "general/terraform.tfstate"
-    region  = "us-east-1"
-  }
-}
