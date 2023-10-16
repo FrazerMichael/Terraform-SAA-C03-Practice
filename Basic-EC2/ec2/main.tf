0aa8742ec2c3c7b3f307f979f95d2de2ba52f4df
Michael Frazer <mjfrazer1@gmail.com>
2023-10-16 19:43:43 -0400

    clean up

--- Basic-EC2/ec2/main.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_instance" "ec2-instance" {
-    ami = var.ami_id
-    instance_type = "t2.micro"
-    key_name = var.key
-    subnet_id = var.subnet_id
-    security_groups = [var.SG]
-}
-
--- Basic-EC2/main.tf
+++
@@ -1,8 +0,0 @@
-module "ec2" {
-  source    = "./ec2"
-  vpc_id    = "vpc-0c3d28c3627ee6b4e"
-  ami_id    = "ami-03a6eaae9938c858c"
-  key       = "general-admin"
-  subnet_id = "subnet-0263d3443ea69b771"
-  SG        = "sg-04eaa03828fed6848"
-}
